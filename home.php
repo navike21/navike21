033aec1d6ff0bcfb4066dc71381efa97734ad028
José Iván Chapoñán Casiano <ivan@navike21.com>
2018-04-15 00:09:13 -0500

    imagen del ultimo post

--- home.php
+++ home.php
@@ -29,8 +29,26 @@
     <div class="w_100 banner_principal" style="background-image: url('.$portada_blog.')"></div>
     <div class="w_80">
         <article class="w_100 section_top_center bienvenida">
-            <h2>Blog</h2>';
+            <h2>Blog</h2>
+            <section class="w_100 section_top_justify">';
+            $args = array(
+                // 'category_name' => 'Pic of the Week',
+                'posts_per_page' => 1,
+                'order_by' => 'date',
+                'order' => 'desc'
+            );
 
+            $post = get_posts( $args );
+            if($post) {
+                $post_id = $post[0]->ID;
+                if(has_post_thumbnail($post_id)){
+                    // use one of these
+                    echo get_the_post_thumbnail($page->ID, 'thumbnail');
+                    echo get_the_post_thumbnail( $post_id, array(80, 80), array('class' => 'post_thumbnail') );
+                }
+            }
+
+    echo'   </section>';
     echo '
         </article>
         <div id="works" class="w_100 section_middle_center">';
